7bfd73fd76cdb6877c04aebbab31560ca38a1584
DickyDicky7 <DickyDicky7@users.noreply.github.com>
2022-05-26 16:06:00 +0700

    add error handler

--- src/Server.hs
+++ src/Server.hs
@@ -4,8 +4,11 @@ module Server
 
 import qualified Control.Concurrent.Async      as Async
 import qualified Data.Aeson                    as JSON
+import qualified Data.Typeable                 as Typeable
 import qualified Network.Wai.Handler.Warp      as HTTP
 import qualified Network.Wai.Logger            as HTTP
+import qualified Network.Wai.Middleware.Servant.Errors
+                                               as Middleware
 import           Servant
 import           Universum
 import qualified Universum.Unsafe              as Unsafe
@@ -17,11 +20,32 @@ data Response result = Response
     }
     deriving (Eq, Show, Generic, JSON.ToJSON, JSON.FromJSON)
 
+data CustomError a
+    deriving Accept via JSON
+
+instance Middleware.HasErrorBody (CustomError JSON) '["status" , "message" , "result"] where
+    encodeError Middleware.StatusCode {..} Middleware.ErrorMsg {..} =
+        JSON.encode @(Response ()) (Response unStatusCode unErrorMsg Nothing)
+
+data OhShit = OhShit
+    deriving (Eq, Show)
+
+onError :: SomeException -> Handler a
+onError error = throwError err500 { errBody = encodeUtf8 (displayException error) }
+
+instance Exception OhShit where
+    displayException _ = "Oh shitz!"
+
 type API_1 = "api1" :> Post '[JSON] (Response ())
 
 server_1 :: Server API_1
-server_1 = throwError err300 { errBody = "This is err 300", errHeaders = [("hihi", "haha")] }
-    >> pure (Response 200 "API_1 was requested" Nothing)
+server_1 =
+    do
+        -- throwM OhShit
+        throwError err418 { errBody = "This is err 300", errHeaders = [("hihi", "haha")] }
+        pure (Response 200 "API_1 was requested" Nothing)
+    `catch` onError
+
 
 app_1 :: Application
 app_1 = serve (Proxy @API_1) server_1
@@ -40,7 +64,7 @@ microServices = [(3000, app_1), (8000, app_2)]
 startMicroService :: (HTTP.Port, Application) -> IO ()
 startMicroService (port, app) = HTTP.withStdoutLogger \logger -> do
     let settings = HTTP.defaultSettings & HTTP.setPort port & HTTP.setLogger logger
-    HTTP.runSettings settings app
+    HTTP.runSettings settings (Middleware.errorMw @(CustomError JSON) @'["status" , "message" , "result"] app)
 
 start :: IO ()
 start = microServices `Async.forConcurrently_` startMicroService
